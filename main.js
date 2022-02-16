d55f0a7c96cf8995dccd15ff9b741dfd22aa8bae
angelinahan <han.ang@northeastern.edu>
2022-02-15 23:16:47 -0500

    styling and code cleanup

--- main.js
+++ main.js
@@ -1,36 +1,25 @@
-
 function circle1Clicked() {
-
-    let text1 = document.getElementById("text1");
-
-    let text = parseInt(text1.innerHTML);
-    text = text + 1;
-
-    text1.innerHTML = text;
-
-    console.log(text1);
+  const text1 = document.getElementById("text1");
+  let count = parseInt(text1.innerHTML);
+  count = count + 1;
+  text1.innerHTML = count;
 }
 
 function addBorder() {
-
-    let circle2 = document.getElementById("circle2");
-    circle2.setAttribute("class", "hasBorder");
-
+  const circle2 = document.getElementById("circle2");
+  circle2.setAttribute("class", "hasBorder");
 }
 
 function removeBorder() {
-    let circle2 = document.getElementById("circle2");
-    circle2.setAttribute("class", "");
+  const circle2 = document.getElementById("circle2");
+  circle2.setAttribute("class", "");
 }
 
 function changeOpacity() {
-
-    let circle3 = document.getElementById("circle3");
-
-    if (circle3.getAttribute("class") === "fullOpacity") {
-        circle3.setAttribute("class", "zeroOpacity");
-        console.log(circle3.className);
-    } else {
-        circle3.setAttribute("class", "fullOpacity");
-    }
+  const circle3 = document.getElementById("circle3");
+  if (circle3.getAttribute("class") === "zeroOpacity") {
+    circle3.setAttribute("class", "fullOpacity hasBorder");
+  } else {
+    circle3.setAttribute("class", "zeroOpacity");
+  }
 }
